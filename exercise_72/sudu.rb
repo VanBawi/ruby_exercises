199c8c5caac5dc0074f0a0fdd14b8c77ca434fe3
Jack <superjackpower85@gmail.com>
2019-07-01 01:46:40 +0800

    sudoku added

--- exercise_72/sudu.rb
+++ exercise_72/sudu.rb
@@ -1,51 +1,35 @@
 require 'byebug'
-# require 'awesome_print'
-
 class Sudoku
-    attr_accessor :board, :units, :peers, :values, :done, :squares, :rows, :cols, :digits, :unit_list
-    # Takes a board as a string in the format
-    # you see in the puzzle file. Returns
-    # something representing a board after
-    # your solver has tried to solve it.
-    # How you represent your board is up to you!
-    # attr_reader :numbers, :rows, :columns, :squares
+    attr_accessor :board, :units, :peers, :values, :done, :squares, :rows, :cols, :digits
     def initialize(board_string)
         @board = board_string.split("")
         @digits = (1..9).to_a
         @rows = ("A".."I").to_a
         @cols = (1..9).to_a
-        #total number of squares
         @squares = cross(@rows, @cols)
 
         @unit_list = []
         col_units = @cols.map {|col|cross(@rows, [col])}
         row_units = @rows.map {|row|cross([row], @cols)}
+
         nine_squares = []
-        # byebug
-        #getting the row unit 3each
         @rows.each_slice(3) do |therow|
             @cols.each_slice(3) do |thecol|
                 nine_squares << cross(therow, thecol)
             end
         end
 
-        #including the row units, column units
         @unit_list = row_units + col_units + nine_squares
-        #create new hash  the units possible for each square
         @units = {}
         for i in @squares do 
             @units[i] = @unit_list.select {|u|u.include?(i)}
-            # byebug
         end 
 
-        #create new hash for all the peers of each square
         @peers = {}
         for i in @squares do 
             @peers[i] = @units[i].flatten.uniq.select {|j| j != i}
-            # byebug
         end
 
-        #creating something to keep the values of each square
         @values = {}
         for i in @squares do
             @values[i] = nil
@@ -55,46 +39,34 @@ class Sudoku
         for i in @squares do
             @done[i] = false
         end
-        # byebug
-        # createdboard(read_grid_str(board_string))
     end
 
         def cross(a,b)
-            return a.map { |x| b.map { |y| :"#{x}#{y}"} }.flatten
-            
+            return a.map { |x| b.map { |y| :"#{x}#{y}"} }.flatten  
         end
-        
         def solve
-           
-        end
 
-        # Returns a boolean indicating whether or not the provided board is solved.
+        end
         def finished?
 
         end
-
         def board
-            index = 0
             @rows.each do |row|
                 @cols.each do |col|
                     @values["{#row}{#col}"] = board_string[index]
+                    index += 1
                 end
-                index += 1
             end
         end
-        # Returns a string representing the current state of the board, well formatted for output to the screen. No `puts` here!
         def to_s
             variable = 0
             if @board % 9 == 0
                 print "." *30
+                variable += 1
             end
-            variable += 1
         end
 
         def read_grid_str(input)
-                # step 3, lets move the integer from array into the read_grid
-                # _str need to initialize 2D array
-                # remember - is empty space
             grid = input.split(//).map {|s| s.gsub("", "-")} #i for 0s not for -dash
             return grid
         end
@@ -104,10 +76,7 @@ class Sudoku
             newarray.each do |x, y|
                 @values[x] = y.to_s
             end
-            return @values
-            # step 4. lets find the number to be filled in 
-            # step 4a, first lets fill in the entire board with all  the possibilities
-            # first assign inital possibilities
+                return @values
         end
                 
         def initial_possibilites
@@ -117,17 +86,11 @@ class Sudoku
                 end
             end
         end
-        #     then step 4b. looking at this, we fill in the ones that only has one possibility left (sure it is correct)
-        #     then we will also continue to loop this and repaet since every time 
-        #     that one number is filled in then the possibilities from all peers will be removed
-        #     loop this until you can no longer find the answer
-
+    
         def reevaluate_board_and_eliminate
             can_continue = false
-            # iteration = 0
             while  can_continue ==false
                 can_continue =true
-                # iteration += 1
                 @squares.each do |squarename|
                     if @done[squarename] = false
                         if @values[squarename].length <= 1
@@ -141,13 +104,9 @@ class Sudoku
         end
 
         def eliminate(square, number)
-            if @peers === @digits
-                @digits - initial_possibilites
-            end 
+            if 
         end
 end
 
-board_string = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
 game = Sudoku.new("board_string")
-p game.board
-# board_string = File.readlines('sudoku_puzzles.txt')
+board_string = File.readlines('sudoku_puzzles.txt')
--- test.rb
+++ test.rb
@@ -1,106 +1,4 @@
-# def print_addition(num1,num2)
-#    puts num1 + num2
-# end
-# return  print_addition(1,1)
-# require 'json'
-
-
-# class Board
-#     attr_reader :tiles, :valid_words
-
-#     def initialize
-#         @tiles = generate_tiles
-#         @valid_words = generate_valid_words
-#     end
-
-#     def check_word(word)
-#         valid_words.include?(word)
-#     end
-
-#     def print_board
-#         puts "Here's the board:"
-#         tiles.each_with_index {|tile, index|
-#             print "#{tile} #{"\n"if index % 4 == 3}"
-#         }
-#     end
-
-#     private
-
-#         def generate_tiles
-#             # https://boardgames.stackexchange.com/q/29264
-#             dice = [
-#                 ['R', 'I', 'F', 'O', 'B', 'X', '*'],
-#                 ['I', 'F', 'E', 'H', 'E', 'Y', '*'],
-#                 ['D', 'E', 'N', 'O', 'W', 'S', '*'],
-#                 ['U', 'T', 'O', 'K', 'N', 'D', '*'],
-#                 ['H', 'M', 'S', 'R', 'A', 'O', '*'],
-#                 ['L', 'U', 'P', 'E', 'T', 'S', '*'],
-#                 ['A', 'C', 'I', 'T', 'O', 'A', '*'],
-#                 ['Y', 'L', 'G', 'K', 'U', 'E', '*'],
-#                 ['QU', 'B', 'M', 'J', 'O', 'A', '*'],
-#                 ['E', 'H', 'I', 'S', 'P', 'N', '*'],
-#                 ['V', 'E', 'T', 'I', 'G', 'N', '*'],
-#                 ['B', 'A', 'L', 'I', 'Y', 'T', '*'],
-#                 ['E', 'Z', 'A', 'V', 'N', 'D', '*'],
-#                 ['R', 'A', 'L', 'E', 'S', 'C', '*'],
-#                 ['U', 'W', 'I', 'L', 'R', 'G', '*'],
-#                 ['P', 'A', 'C', 'E', 'M', 'D', '*'],
-#             ]
-
-#             # To enable * use rand(7)
-#             tiles = dice.map {|sides| sides[Random.new.rand(6)]}
-#         end
-
-#         def generate_valid_words
-#             dictionary = File.read("lib/board/dictionary.txt").split("\n")
-#             valid_words = dictionary.select {|word| find_word(tiles, word)}
-#         end
-
-#         # Given a tile's index (0 - 15), return a list of adjacent tile indexes
-#         def get_adjacent_indexes(index)
-#             row = index / 4  # Ruby will round-down integer division
-#             col = index % 4
-
-#             adjacent_indexes = []
-#             adjacent_indexes.push(index - 4) if row > 0  # top
-#             adjacent_indexes.push(index + 4) if row < 3  # bottom
-#             adjacent_indexes.push(index - 1) if col > 0  # left
-#             adjacent_indexes.push(index + 1) if col < 3  # right
-#             adjacent_indexes.push(index - 5) if row > 0 && col > 0  # top-left
-#             adjacent_indexes.push(index - 3) if row > 0 && col < 3 # top-right
-#             adjacent_indexes.push(index + 3) if row < 3 && col > 0  # bottom-left
-#             adjacent_indexes.push(index + 5) if row < 3 && col < 3  # bottom-right
-
-#             return adjacent_indexes
-#         end
-
-
-#         def find_next_letter(board, tile, tile_index, remaining_word, used_indexes)
-#             # puts "Checking for \"#{remaining_word}\" from #{tile} at #{tile_index}..."
-#             return true if remaining_word.length == 0
-
-#             adjacent_indexes = get_adjacent_indexes(tile_index)
-#             adjacent_indexes.each do |index|
-#                 # to prevent reverse traversal
-#                 next if used_indexes.include?(index)
-
-#                 can_form_remaining = false
-#                 if board[index] == remaining_word[0] || board[index] == "*"
-#                     used_indexes.push(index)
-#                     can_form_remaining = find_next_letter(board, board[index], index, remaining_word[1..-1], used_indexes)
-#                 end
-
-#                 return true if can_form_remaining
-#             end
-
-#             return false
-#         end
-
-
-#         def find_word(board, word)
-#             word = word.upcase  # to prevent any casing issues
-
-# def first
+#def first
 #     return "Jack"
 # end
 # def last
@@ -261,21 +159,105 @@
 #     end
 # end
 
-
-
-
 # phone = Gadget.new
 # puts phone.to_s
 # puts phone.username
 # puts phone.username=("Rubyman")
 # puts phone.production_number
 # puts phone.password = "bestpasswordever"
-    
-
-
+  
 # 60 
 # 67
 # 61
 # 62
 # 66
 # 68
+
+
+# class Sudoku
+#     def initialize board
+#       @board = board
+#     end
+  
+#     def solve!
+#       num_index = @board.index('0')
+#       return true unless num_index
+#       ('1'..'9').each do |possibility|
+#         @board[num_index] = possibility
+#         return @board if (board_valid? num_index) && solve!
+#       end
+#       @board[num_index] = '0'
+#       false
+#     end
+  
+#     def board_valid? num_index
+#       num = @board[num_index]
+#       return false unless valid_row? num, num_index
+#       return false unless valid_col? num, num_index
+#       return false unless valid_box? num, num_index
+#       true
+#     end
+  
+#     def valid_row? num, num_index
+#       row = num_index / 9
+#       start = row * 9
+#       (start..(start + 8)).each do |check_i|
+#         return false unless valid? check_i, num, num_index
+#       end
+#       true
+#     end
+  
+#     def valid_col? num, num_index
+#       col = num_index%9
+#       start = 0
+#       (1..9).each do |x|
+#         check_i = start + col
+#         return false unless valid? check_i, num, num_index
+#         start += 9
+#       end
+#       true
+#     end
+  
+#     def valid_box? num, num_index
+#       col_start, row_start = (((num_index%9)/3) * 3), ((num_index/27) * 27)
+#       3.times do
+#         (col_start..(col_start+2)).each do |col|
+#           return false unless valid? col + row_start, num, num_index
+#         end
+#         row_start += 9
+#       end
+#     end
+  
+#     def valid? index, num, num_index
+#       return false if index != num_index && num == @board[index]
+#       true
+#     end
+#   end
+  
+#   s = Sudoku.new("800000000003600000070090200050007000000045700000100030001000068008500010090000400")
+#   p s.solve!
+
+# class Gadget
+#     def initialize
+#     @username = "User #{rand(1..100)}"
+#     @password = "Topsecret"
+#     @production_number = "#{("A".."Z").to_a.sample}-#{rand(1..100)}"
+#     end
+
+#     def to_s
+#         "Gadget #{@production_number}"
+#     end
+
+#     def username
+#         @username
+#     end
+#     def production_number
+#         @production_number
+#     end
+#     def username=(new_username)
+#         @username = new_username
+#     end
+#     def password=(new_password)
+#         @password = new_password
+#     end
+# end
